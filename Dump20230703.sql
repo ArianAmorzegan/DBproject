eec0a1ab0f39737fe2225e4f02d092a5ffb23683
Arian <arian.amorzegan@mail.um.ac.ir>
2023-07-06 09:30:43 +0330

    modify tables

--- Dump20230703.sql
+++ Dump20230703.sql
@@ -183,6 +183,7 @@ DROP TABLE IF EXISTS `Instructor`;
 CREATE TABLE `Instructor` (
   `id` varchar(10) NOT NULL,
   `room_no` int DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_Instructor_1` FOREIGN KEY (`id`) REFERENCES `Person` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -194,7 +195,7 @@ CREATE TABLE `Instructor` (
 
 LOCK TABLES `Instructor` WRITE;
 /*!40000 ALTER TABLE `Instructor` DISABLE KEYS */;
-INSERT INTO `Instructor` VALUES ('4567891234',5),('5',10),('7',4),('8',2);
+INSERT INTO `Instructor` VALUES ('4567891234',5,NULL),('5',10,NULL),('7',4,NULL),('8',2,NULL);
 /*!40000 ALTER TABLE `Instructor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -225,7 +226,7 @@ CREATE TABLE `Person` (
 
 LOCK TABLES `Person` WRITE;
 /*!40000 ALTER TABLE `Person` DISABLE KEYS */;
-INSERT INTO `Person` VALUES ('',NULL,NULL,NULL,NULL,NULL,NULL),('1234567891','arian','amorzegan',21,'','',0),('2345678912','iman','ras',20,'','',1),('3456789123','nima','alavi',22,'','',0),('4567891234','ali','hadad',45,NULL,NULL,2),('5','kamran','ads',55,'','',0),('6','mohammadreza','zaman',22,NULL,NULL,0),('7','alireza','taleb',32,NULL,NULL,1),('8','reza','babayi',52,NULL,NULL,0);
+INSERT INTO `Person` VALUES ('',NULL,NULL,NULL,NULL,NULL,NULL),('1234567891','kamran','amorzegan',23,'','',0),('2345678912','iman','ras',20,'','',1),('3456789123','nima','alavi',32,'','',0),('4567891234','ali','hadad',45,NULL,NULL,2),('5','kamran','ads',55,'','',0),('6','mohammadreza','zaman',32,NULL,NULL,0),('7','alireza','taleb',32,NULL,NULL,1),('8','meeoosa','babayi',29,NULL,NULL,0);
 /*!40000 ALTER TABLE `Person` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -276,11 +277,14 @@ CREATE TABLE `Section_Student` (
   `semstuid` int DEFAULT NULL,
   `status` tinyint DEFAULT NULL,
   `Score` int DEFAULT NULL,
+  `stid` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_Section_Student_2_idx` (`sectid`),
   KEY `fk_Section_Student_1_idx` (`semstuid`),
+  KEY `fk_Section_Student_3_idx` (`stid`),
   CONSTRAINT `fk_Section_Student_1` FOREIGN KEY (`semstuid`) REFERENCES `Semester_Student` (`idSemester_Student`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `fk_Section_Student_2` FOREIGN KEY (`sectid`) REFERENCES `Section` (`id`)
+  CONSTRAINT `fk_Section_Student_2` FOREIGN KEY (`sectid`) REFERENCES `Section` (`id`),
+  CONSTRAINT `fk_Section_Student_3` FOREIGN KEY (`stid`) REFERENCES `Student` (`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -290,7 +294,7 @@ CREATE TABLE `Section_Student` (
 
 LOCK TABLES `Section_Student` WRITE;
 /*!40000 ALTER TABLE `Section_Student` DISABLE KEYS */;
-INSERT INTO `Section_Student` VALUES ('1','1',1,1,0),('10','4',5,1,15),('11','4',5,1,18),('2','1',4,0,0),('3','2',1,1,15),('4','4',1,1,18),('5 ','2',1,1,14),('6','2',1,1,18),('7','3',4,1,14),('8','1',1,1,12),('9','4',5,1,18);
+INSERT INTO `Section_Student` VALUES ('1','1',1,1,0,NULL),('10','4',5,1,15,NULL),('11','4',5,1,18,NULL),('12','1',1,0,0,NULL),('13','3',1,0,0,NULL),('2','1',4,0,0,NULL),('3','2',1,1,15,NULL),('4','4',1,1,18,NULL),('5 ','2',1,1,14,NULL),('6','2',1,1,18,NULL),('7','3',4,1,14,NULL),('8','1',1,1,12,NULL),('9','4',5,1,18,NULL);
 /*!40000 ALTER TABLE `Section_Student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -308,6 +312,7 @@ CREATE TABLE `Semester_Student` (
   `avg` int DEFAULT NULL,
   `year` int DEFAULT NULL,
   `semester` tinyint DEFAULT NULL,
+  `units_take` int DEFAULT '0',
   PRIMARY KEY (`idSemester_Student`),
   KEY `fk_Semester_Student_1_idx` (`stuid`),
   CONSTRAINT `fk_Semester_Student_1` FOREIGN KEY (`stuid`) REFERENCES `Student` (`sid`)
@@ -320,7 +325,7 @@ CREATE TABLE `Semester_Student` (
 
 LOCK TABLES `Semester_Student` WRITE;
 /*!40000 ALTER TABLE `Semester_Student` DISABLE KEYS */;
-INSERT INTO `Semester_Student` VALUES (1,'9922762399',8,0,1401,1),(2,'9922762399',12,15,1401,2),(3,'9922762399',16,14,1402,1),(4,'9922752487',14,12,1401,1),(5,'9922752487',22,14,1401,2);
+INSERT INTO `Semester_Student` VALUES (1,'9922762399',8,0,1401,1,6),(2,'9922762399',12,15,1401,2,0),(3,'9922762399',16,14,1402,1,0),(4,'9922752487',14,12,1401,1,0),(5,'9922752487',22,14,1401,2,0);
 /*!40000 ALTER TABLE `Semester_Student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -362,4 +367,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-03  5:45:02
+-- Dump completed on 2023-07-06  9:30:30
