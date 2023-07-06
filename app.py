1c56cc2c179ebe298677eda086aa2a9fa09eb4be
Arian <arian.amorzegan@mail.um.ac.ir>
2023-07-06 16:08:42 +0330

    add delete section

--- app.py
+++ app.py
@@ -34,6 +34,32 @@ def update_student():
         conn.commit()
     
     return jsonify({'message': 'Student updated successfully'})
+@app.route('/showsection/', methods=['GET'])
+def show_section():
+    query_params = request.args
+    sid = query_params.get('sid')
+    query =f''' SELECT class_no ,Course.name , Department.dname, Person.fname ,Person.lname FROM University.Section,Course, Instructor,Person , Department where Section.courseid = Course.id AND instructorid = Instructor.id AND Course.did = Department.did AND Instructor.id = Person.id AND Section.id  = {str(sid)} ;'''    
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return jsonify(result)
+@app.route('/showexam/', methods=['GET'])
+def show_exam():
+    query_params = request.args
+    sid = query_params.get('sid')
+    query =f''' SELECT final_date , Course.name FROM Section , Section_Student , Course WHERE Section_Student.stid = {str(sid)} AND Section_Student.sectid = Section.id AND Section.courseid = Course.id;'''
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return jsonify(result)
+
+@app.route('/deletesection/', methods=['DELETE'])
+def delete_section():
+    query_params = request.args
+    sid = query_params.get('sid')
+    sectid = query_params.get('sectid')
+    query =f''' DELETE FROM Section_Student WHERE Section_Student.stid = "{sid}" AND Section_Student.sectid = "{sectid}";'''
+    cursor.execute(query)
+    conn.commit()
+    return jsonify({'message': 'Section deleted successfully'})
 
 if __name__ == '__main__':
     app.run()
